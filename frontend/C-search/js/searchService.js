18d4d11f3ad740df3b65cfcaeb21882467c32837
tsingleton5 <tjsingleton5@gmail.com>
2016-07-08 11:11:57 -0600

    some media queries

--- frontend/C-search/js/searchService.js
+++ frontend/C-search/js/searchService.js
@@ -17,12 +17,24 @@ this.addFavProb = function (problem) {
   return $http({
     method: 'PUT',
     url: '/fav',
-    data: {favorite: problem} // expecting an object not a primitive 
+    data: {favorite: problem} // expecting an object not a primitive
   })
   .then(function (response) {
     return response.data;
   })
 }
+
+// this.catQuote = function () {
+//   return $http({
+//     method: 'GET',
+//     url: 'http://catfacts-api.appspot.com/api/facts?number=4'
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//     return response
+//   })
+// }
+// this.catQuote();
 
 //ending
 })
